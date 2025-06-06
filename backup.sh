dbecf8654b01d5457ab44ea9791e610e106af6f1
Shreyashi Deb Roy <114597118+SHREYASHIKLS28@users.noreply.github.com>
2025-06-07 02:57:31 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rclone sync /mnt/c/Users/91841/Documents gdrive:DocumentsBackup --log-file=/mnt/c/Users/Username/backup.log --log-level INFO
+rclone sync /mnt/c/Users/Username/Documents gdrive:DocumentsBackup --log-file=/mnt/c/Users/Username/backup.log --log-level INFO
 if [ $? -ne 0 ]; then
     echo "Backup FAILED at $(date)" >> /mnt/c/Users/Username/backup_error.log
     # Call your alert function here
